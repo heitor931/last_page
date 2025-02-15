3cbc19d8966687784708098cd76326dd30d5693e
Heitor Paulo <heitorino1@gmail.com>
2025-02-15 23:46:32 +0100

    Updated code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ books = get_all_books()
 current_date_time = f"{date.today()}-{datetime.now().hour}:{datetime.now().minute}"  
 
 
-if len(args) == 1:
+if args[1] == 'list':
     # Get updated data from database
     print("Fetching the latest data...")
 
@@ -25,28 +25,55 @@ if len(args) == 1:
         colors=[pt.Colors.white, pt.Colors.red, pt.Colors.yellow, pt.Colors.blue, pt.Colors.cyan, pt.Colors.green, pt.Colors.purple,pt.Colors.black],
     )
     # )
-    print("-" * 97)
-
+    print("-" * 120)
     print(table)
-
-    print("-" * 97)
+    print("-" * 120)
 
     # Get the latest data
 elif args[1] == "update":
+    if len(args) == 2:
+        for book in books:
+            choice = (input(f"{book['book_name']} - [Current page]: "))
+            if not choice:
+                continue
+            else:
+                sanitized_choice = int(choice)
+            try:
+                book["current_page"] = sanitized_choice
+                book["last_page_date"] = current_date_time
+                if book['last_page_date'] == book['total_pages']:
+                    book['completed'] = True
+                post_to_database(book)
+                print("Updated successfully!")
+            except ValueError as e:
+                print('Not a valid choice')
+    elif len(args) >= 3:
+        indexes = args[2:]
+        print(indexes)
+        for idx in indexes:
+            if int(idx) in [x['id'] for x in books]:
+                for book in books:
+                    if int(idx) == book['id']:
+                        try:
+                            choice = (input(f"{book['book_name']} - [Current page]: "))
+                            if not choice:
+                                continue
+                            else:
+                                sanitized_choice = int(choice)
+                            book["current_page"] = sanitized_choice
+                            book["last_page_date"] = current_date_time
+                            if book['last_page_date'] == book['total_pages']:
+                                book['completed'] = True
+                            post_to_database(book)
+                            print("Updated successfully!")
+                            break
+                        except ValueError as e:
+                            print('Not a valid choice')
+            else:
+                print("There is no book with this Id")
 
-    for book in books:
-        choice = int(input(f"{book['book_name']} Current page: "))
-        if choice:
-            book["current_page"] = choice
-            book["last_page_date"] = current_date_time
-            if book['last_page_date'] == book['total_pages']:
-                book['completed'] = True
-
-    for book in books:
-        post_to_database(book)
-    print("Updated successfully!")
-
-elif args[1] == "create":
+# Create new Books for reading
+elif args[1] == "add":
     track_list = books.copy()
     while True:
         book_name = input("What is the name of the Book? ")
@@ -69,14 +96,18 @@ elif args[1] == "create":
         track_list.append(book)
         insert_to_database(book)
         if more.lower() == 'n':
+            get_all_books()
             break
 
+# Delete books from the list
+
+# Usage of the application
 elif args[1].lower() == "usage":
     print("USAGE:")
     print("=" * 30)
     print("Without arguments, get updated page numbers")
-    print("options: update | create")
-    print("update: Update the page numbers of the books")
+    print("options: list | add | update | to_excel | to_pdf")
+    print("update: update the page numbers of the books")
     print("create: Create new books")
     
 
